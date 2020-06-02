182d634f47b933ca02e3f37dcd8a0ddd79e8b287
Allan Melo <allmelo@gmail.com>
2020-06-02 14:20:50 -0300

    Setup Sqlite 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,37 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
-
-const users = [
-    'Diego',
-    'Robson',
-    'Cleiton',
-    'Joelma'
-];
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-    const filteredUsers = search ? users.filter( user => user.includes(search) ) : users;
-    response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const user = users[id];
-
-    response.json(user);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-    console.log(data);
-
-    const user = {
-        user: data.name,
-        email: data.email
-    };
-    return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333); 
